da0344cb6289043628c595ca1291bb691ac7e7a3
luyao <luyao@mw-robot.com>
2023-05-25 19:13:41 +0800

    refactor: 删除test

--- packages/pss/test.js
+++
@@ -1,6 +0,0 @@
-import ora from 'ora'
-import chalk from 'chalk'
-
-const spinner = ora('Loading unicorns').start()
-spinner.succeed(chalk.green('unicorns loaded'))
-spinner.fail(chalk.red('unicorns loaded'))
